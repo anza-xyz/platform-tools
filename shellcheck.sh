f6c8958c80a28dc68b5ef99f9860bb291c5c7d66
Jack May <jack@solana.com>
2019-02-25 11:59:54 -0800

    Update shellcheck.sh

--- shellcheck.sh
+++ shellcheck.sh
@@ -8,9 +8,9 @@ cd "$(dirname "$0")"
 set -x
 docker pull koalaman/shellcheck
 find . -name "*.sh" \
-       -not -regex ".*/llvm/.*" \
+       -not -regex ".*/rust/.*" \
     -print0 \
   | xargs -0 \
-      docker run --workdir /llvm-builder --volume "$PWD:/llvm-builder" --rm koalaman/shellcheck --color=always --external-sources --shell=bash
+      docker run --workdir /workdir --volume "$PWD:/workdir" --rm koalaman/shellcheck --color=always --external-sources --shell=bash
 
 exit 0
