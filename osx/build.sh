ed7afd70b8a5b06ee20872768118bf25e214e6e1
Jack May <jack@solana.com>
2020-03-02 15:54:28 -0800

    Build matching cargo

--- osx/build.sh
+++ osx/build.sh
@@ -10,14 +10,22 @@ cd out
 git clone --single-branch --branch solana-1.39 https://github.com/solana-labs/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
+git clone --single-branch --branch rust-1.39.0 https://github.com/rust-lang/cargo.git
+echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
+
 pushd rust
 ./build.sh
+popd
+
+pushd cargo
+cargo build --release
 popd
 
 # Copy build products
 mkdir -p deploy
 cp version.md deploy
 cp -rf rust/build/x86_64-apple-darwin/stage1/ deploy
+cp cargo/target/release/cargo deploy/bin
 
 # Needed by xargo
 mkdir deploy/lib/rustlib/x86_64-apple-darwin/bin
