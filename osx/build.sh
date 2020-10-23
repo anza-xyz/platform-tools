61d2eedfb77afe431cb2269b15a36846e0fb5134
Jack May <jack@solana.com>
2020-10-23 11:51:20 -0700

    Remove platform dependent library dependency

--- osx/build.sh
+++ osx/build.sh
@@ -18,16 +18,17 @@ pushd rust
 popd
 
 pushd cargo
+export OPENSSL_STATIC=1
 cargo build --release
 popd
 
 # Copy build products
-mkdir -p deploy
+mkdir -p deploy/bin
 cp version.md deploy
 cp -rf rust/build/x86_64-apple-darwin/stage1/ deploy
-cp cargo/target/release/cargo deploy/bin
+cp cargo/target/release/cargo deploy/bin/
 
 # Needed by xargo
-mkdir deploy/lib/rustlib/x86_64-apple-darwin/bin
+mkdir -p deploy/lib/rustlib/x86_64-apple-darwin/bin
 
 tar -C deploy -jcf solana-rust-bpf-osx.tar.bz2 .
