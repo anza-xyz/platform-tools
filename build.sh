3a56b3a462a6b5f84b7c30c3aafb7050dd0af43b
Jack May <jack@solana.com>
2021-02-17 12:33:52 -0800

    Build rust and llvm from single repo

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ rm -rf deploy
 mkdir -p deploy
 
 (cd linux && ./build.sh )
-cp linux/out/solana-rust-bpf-linux.tar.bz2 deploy
+cp linux/out/solana-bpf-tools-linux.tar.bz2 deploy
 
 if [ "$(uname)" == "Darwin" ]; then
   (cd osx && ./build.sh )
-  cp osx/out/solana-rust-bpf-osx.tar.bz2 deploy
+  cp osx/out/solana-bpf-tools-osx.tar.bz2 deploy
 else
   echo Warning: Local machine not a Mac, cannot build osx variant, skipping
 fi
