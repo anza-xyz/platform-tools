99393462a8b2bc233e95f66cbbc5ffae6091c0e8
Jack May <jack@solana.com>
2021-02-17 12:33:52 -0800

    Build rust and llvm from single repo

--- linux/build.sh
+++ linux/build.sh
@@ -3,24 +3,43 @@ set -ex
 
 cd "$(dirname "$0")"
 
-docker build --no-cache -t solanalabs/rust-bpf .
+docker build -t solanalabs/bpf-tools .
+# docker build --no-cache -t solanalabs/bpf-tools .
+id=$(docker create solanalabs/bpf-tools)
 
 rm -rf out
 mkdir -p out
 cd out
 
-# Copy out and bundle release products
-mkdir -p deploy
-id=$(docker create solanalabs/rust-bpf)
-docker cp "$id":/usr/local/rust_version.md deploy
-docker cp "$id":/usr/local/cargo_version.md deploy
-docker cp "$id":/usr/local/bin deploy
-docker cp "$id":/usr/local/lib deploy
+# Copy rust build products
+mkdir -p deploy/rust
+docker cp "$id":/usr/local/rust/rust_version.md deploy/rust
+docker cp "$id":/usr/local/rust/cargo_version.md deploy/rust
+docker cp "$id":/usr/local/rust/bin deploy/rust
+docker cp "$id":/usr/local/rust/lib deploy/rust
+mkdir -p deploy/rust/lib/rustlib/x86_64-unknown-linux-gnu/bin # Needed by xargo
+
+# Copy llvm build products
+mkdir -p deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/clang deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/clang++ deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/clang-10 deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/clang-cl deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/clang-cpp deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/ld.lld deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/ld64.lld deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llc deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/lld deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/lld-link deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llvm-ar deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llvm-objcopy deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llvm-objdump deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llvm-readelf deploy/llvm/bin
+docker cp "$id":/usr/local/llvm/bin/llvm-readobj deploy/llvm/bin
+mkdir -p deploy/llvm
+docker cp "$id":/usr/local/llvm/lib deploy/llvm
+
 docker rm -v "$id"
 
-# Needed by xargo
-mkdir -p deploy/lib/rustlib/x86_64-unknown-linux-gnu/bin
+tar -C deploy -jcf solana-bpf-tools-linux.tar.bz2 .
 
-tar -C deploy -jcf solana-rust-bpf-linux.tar.bz2 .
-
-# docker push solanalabs/rust-bpf
--- osx/build.sh
+++ osx/build.sh
@@ -3,33 +3,54 @@ set -ex
 
 cd "$(dirname "$0")"
 
-rm -rf out
-mkdir -p out
+# rm -rf out
+# mkdir -p out
 cd out
 
-git clone --single-branch --branch solana-1.46 https://github.com/solana-labs/rust.git
-echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
+# git clone --single-branch --branch solana-1.46 https://github.com/solana-labs/rust.git
+# echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
-# note, branch v1.46.0, v1.47.0, and v1.48.0 fail to build
-git clone --single-branch --branch rust-1.49.0 https://github.com/rust-lang/cargo.git
-echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
+# # note, branch v1.46.0, v1.47.0, and v1.48.0 fail to build
+# git clone --single-branch --branch rust-1.49.0 https://github.com/rust-lang/cargo.git
+# echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
 
-pushd rust
-./build.sh
-popd
+# pushd rust
+# ./build.sh --llvm
+# popd
 
-pushd cargo
-export OPENSSL_STATIC=1
-cargo build --release
-popd
+# pushd cargo
+# export OPENSSL_STATIC=1
+# cargo build --release
+# popd
 
-# Copy build products
-mkdir -p deploy/bin
+# TODO remove
+rm -rf deploy
+
+# Copy rust build products
+mkdir -p deploy/rust/bin
 cp version.md deploy
-cp -rf rust/build/x86_64-apple-darwin/stage1/ deploy
-cp cargo/target/release/cargo deploy/bin/
+cp -rf rust/build/x86_64-apple-darwin/stage1/* deploy/rust
+cp cargo/target/release/cargo deploy/rust/bin/
+mkdir -p deploy/rust/lib/rustlib/x86_64-apple-darwin/bin # Needed by xargo
 
-# Needed by xargo
-mkdir -p deploy/lib/rustlib/x86_64-apple-darwin/bin
+# Copy llvm build products
+mkdir -p deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/clang deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/clang++ deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/clang-10 deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/clang-cl deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/clang-cpp deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/ld.lld deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/ld64.lld deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llc deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/lld deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/lld-link deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llvm-ar deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llvm-objcopy deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llvm-objdump deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llvm-readelf deploy/llvm/bin
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/bin/llvm-readobj deploy/llvm/bin
+mkdir -p deploy/llvm/lib
+cp -rf rust/build/x86_64-apple-darwin/llvm/build/lib/clang deploy/llvm/lib
 
-tar -C deploy -jcf solana-rust-bpf-osx.tar.bz2 .
+tar -C deploy -jcf solana-bpf-tools-osx.tar.bz2 .
