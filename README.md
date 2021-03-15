2a5354ec0b34b092029ddb5e059e83ccbde5cd6d
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-03-15 20:52:46 +0100

    Unify linux and macos scripts

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
-# Customized Rust binaries for Solana that support Berkley Packer Filter (BPF)
+# Customized Rust/Clang binaries for Solana that support Berkley Packer Filter (BPF)
 
 [![Build Status](https://travis-ci.org/solana-labs/rust-bpf-builder.svg?branch=master)](https://travis-ci.org/solana-labs/rust-bpf-builder)
 
 Builds Rust binaries that incorporate customizations and fixes required
 by Solana but not yet upstreamed into Rust or LLVM.
 
-* Builds Rust for Linux (Debian)
+* Builds Rust for Linux (Debian) natively, or in Docker if runs on MacOS
 * Builds Rust for MacOS natively therefore skipped if not building on a Mac
-* Results in tarballs in `/deploy` that can be released
+* Results in tarballs in `out/` that can be released
 
 ### Building
 
 ```bash
-$ ./build.sh
+$ ./build.sh [--docker]
 ```
 
-* Builds Rust for Linux in Docker, tags and pushes `solanalabs/rust`
-* Copies Rust for Linux out of Docker the zips the products into `/deploy`
-* Builds Rust for MacOS natively and zips the products into `/deploy`
+The `--docker` option can be used to build Linux binaries on macOS in
+a docker container.  If the option is not specified only macOS
+binaries are built on a Mac.  On Linux the `--docker` option is
+ignored.
 
 ### Releases
 
 This repo depends on the following:
 
 * https://github.com/solana-labs/rust
+* https://github.com/solana-labs/rust-bpf-sysroot
 
-Any changes that need to go into a Rust release must be made in the appropriate repos listed above.
+Any changes that need to go into a Rust release must be made in the
+appropriate repos listed above.
 
-* See `linux/Dockerfile` for an example of how to sync and build for Linux
-* See `macos/build.sh` for an example of how to sync and build for MacOS)
+* See `build.sh` for an example of how to sync and build
+
+This repository is used to build the toolchain binaries in GitHub
+Actions.  The created tarballs are uploaded as build artifacts in
+GitHub Actions.
--- build.sh
+++ build.sh
@@ -1,15 +1,83 @@
 #!/usr/bin/env bash
 set -ex
 
-rm -rf deploy
-mkdir -p deploy
-
-(cd linux && ./build.sh )
-cp linux/out/solana-bpf-tools-linux.tar.bz2 deploy
-
 if [ "$(uname)" == "Darwin" ]; then
-  (cd osx && ./build.sh )
-  cp osx/out/solana-bpf-tools-osx.tar.bz2 deploy
+    HOST_TRIPLE=x86_64-apple-darwin
+    ARTIFACT=solana-bpf-tools-osx.tar.bz2
 else
-  echo Warning: Local machine not a Mac, cannot build osx variant, skipping
+    HOST_TRIPLE=x86_64-unknown-linux-gnu
+    ARTIFACT=solana-bpf-tools-linux.tar.bz2
+fi
+
+cd "$(dirname "$0")"
+
+rm -rf out
+mkdir -p out
+pushd out
+
+git clone --single-branch --branch solana-1.50 https://github.com/solana-labs/rust.git
+echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
+
+git clone --recurse-submodules --single-branch --branch v1.1 https://github.com/solana-labs/rust-bpf-sysroot.git
+echo "$( cd rust-bpf-sysroot && git rev-parse HEAD )  https://github.com/solana-labs/rust-bpf-sysroot.git" >> version.md
+
+git clone --single-branch --branch rust-1.50.0 https://github.com/rust-lang/cargo.git
+echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
+
+pushd rust
+./build.sh --llvm
+popd
+
+pushd rust-bpf-sysroot
+./test/build.sh ../rust/build/${HOST_TRIPLE}
+popd
+
+pushd cargo
+OPENSSL_STATIC=1 cargo build --release
+popd
+
+# Copy rust build products
+mkdir -p deploy/rust/lib/rustlib/bpfel-unknown-unknown/lib
+cp version.md deploy/
+cp -R rust/build/${HOST_TRIPLE}/stage1/{bin,lib} deploy/rust/
+cp -R rust-bpf-sysroot/test/dependencies/xargo/lib/rustlib/bpfel-unknown-unknown/lib/*.rlib deploy/rust/lib/rustlib/bpfel-unknown-unknown/lib/
+cp -R cargo/target/release/cargo deploy/rust/bin/
+rm -rf deploy/rust/lib/rustlib/src
+
+# Copy llvm build products
+mkdir -p deploy/llvm/{bin,lib}
+while IFS= read -r f
+do
+    cp -R "rust/build/${HOST_TRIPLE}/llvm/build/bin/${f}" deploy/llvm/bin/
+done < <(cat <<EOF
+clang
+clang++
+clang-11
+clang-cl
+clang-cpp
+ld.lld
+ld64.lld
+llc
+lld
+lld-link
+llvm-ar
+llvm-objcopy
+llvm-objdump
+llvm-readelf
+llvm-readobj
+EOF
+         )
+cp -R rust/build/${HOST_TRIPLE}/llvm/build/lib/clang deploy/llvm/lib/
+
+tar -C deploy -jcf ${ARTIFACT} .
+popd
+
+# Build linux binaries on macOS in docker
+if [[ "$(uname)" == "Darwin" ]] && [[ $# == 1 ]] && [[ "$1" == "--docker" ]] ; then
+    docker system prune -a -f
+    docker build -t solanalabs/bpf-tools .
+    id=$(docker create solanalabs/bpf-tools /build.sh)
+    docker cp build.sh "${id}:/"
+    docker start -a "${id}"
+    docker cp "${id}:out/solana-bpf-tools-linux.tar.bz2" out/
 fi
