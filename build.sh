97c32a4caaa777a37a7cac7f64630007d93b8e05
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-07-05 14:37:16 -0700

    Release bpf-tools v1.13
    Update Rust to 1.53.0

--- build.sh
+++ build.sh
@@ -15,10 +15,10 @@ rm -rf out
 mkdir -p out
 pushd out
 
-git clone --single-branch --branch bpf-tools-v1.12 https://github.com/solana-labs/rust.git
+git clone --single-branch --branch bpf-tools-v1.13 https://github.com/solana-labs/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
-git clone --single-branch --branch rust-1.52.0 https://github.com/rust-lang/cargo.git
+git clone --single-branch --branch rust-1.53.0 https://github.com/rust-lang/cargo.git
 echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
 
 pushd rust
