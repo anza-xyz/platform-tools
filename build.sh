2d4868d3a77062f02b026bcee7537d5ce8d85f1a
Jon Cinque <jon.cinque@gmail.com>
2021-09-27 15:47:27 +0200

    Add Windows build job and support in build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ "$(uname)" == "Darwin" ]; then
-    HOST_TRIPLE=x86_64-apple-darwin
-    ARTIFACT=solana-bpf-tools-osx.tar.bz2
-else
-    HOST_TRIPLE=x86_64-unknown-linux-gnu
-    ARTIFACT=solana-bpf-tools-linux.tar.bz2
-fi
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        EXE_SUFFIX=
+        HOST_TRIPLE=x86_64-unknown-linux-gnu
+        ARTIFACT=solana-bpf-tools-linux.tar.bz2;;
+    Darwin*)
+        EXE_SUFFIX=
+        HOST_TRIPLE=x86_64-apple-darwin
+        ARTIFACT=solana-bpf-tools-osx.tar.bz2;;
+    MINGW*)
+        EXE_SUFFIX=.exe
+        HOST_TRIPLE=x86_64-pc-windows-msvc
+        ARTIFACT=solana-bpf-tools-windows.tar.bz2;;
+    *)
+        EXE_SUFFIX=
+        HOST_TRIPLE=x86_64-unknown-linux-gnu
+        ARTIFACT=solana-bpf-tools-linux.tar.bz2
+esac
 
 cd "$(dirname "$0")"
 
@@ -15,7 +27,7 @@ rm -rf out
 mkdir -p out
 pushd out
 
-git clone --single-branch --branch bpf-tools-v1.15 https://github.com/solana-labs/rust.git
+git clone --single-branch --branch bpf-tools-v1.16 https://github.com/solana-labs/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
 git clone --single-branch --branch rust-1.54.0 https://github.com/rust-lang/cargo.git
@@ -32,19 +44,20 @@ popd
 # Copy rust build products
 mkdir -p deploy/rust
 cp version.md deploy/
-cp -R rust/build/${HOST_TRIPLE}/stage1/{bin,lib} deploy/rust/
-cp -R cargo/target/release/cargo deploy/rust/bin/
-rm -rf deploy/rust/lib/rustlib/src
+cp -R rust/build/${HOST_TRIPLE}/stage1/bin deploy/rust/
+mkdir -p deploy/rust/lib/rustlib/
+cp -R rust/build/${HOST_TRIPLE}/stage1/lib/rustlib/${HOST_TRIPLE} deploy/rust/lib/rustlib/
+cp -R rust/build/${HOST_TRIPLE}/stage1/lib/rustlib/bpfel-unknown-unknown deploy/rust/lib/rustlib/
+cp -R cargo/target/release/cargo${EXE_SUFFIX} deploy/rust/bin/
 
 # Copy llvm build products
 mkdir -p deploy/llvm/{bin,lib}
 while IFS= read -r f
 do
-    cp -R "rust/build/${HOST_TRIPLE}/llvm/build/bin/${f}" deploy/llvm/bin/
+    cp -R "rust/build/${HOST_TRIPLE}/llvm/build/bin/${f}${EXE_SUFFIX}" deploy/llvm/bin/
 done < <(cat <<EOF
 clang
 clang++
-clang-12
 clang-cl
 clang-cpp
 ld.lld
