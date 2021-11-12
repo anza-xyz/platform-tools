cc9db0a142590721105ac2385cb47f0e98aa2711
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-11-11 21:47:20 -0800

    Release bpf-tools v1.19
    Upgrade Rust toolchain to v1.56.0 and clang toolchain to v13.0

--- build.sh
+++ build.sh
@@ -23,10 +23,10 @@ rm -rf out
 mkdir -p out
 pushd out
 
-git clone --single-branch --branch bpf-tools-v1.18 https://github.com/solana-labs/rust.git
+git clone --single-branch --branch bpf-tools-v1.19 https://github.com/solana-labs/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
-git clone --single-branch --branch rust-1.54.0 https://github.com/rust-lang/cargo.git
+git clone --single-branch --branch rust-1.56.0 https://github.com/rust-lang/cargo.git
 echo "$( cd cargo && git rev-parse HEAD )  https://github.com/rust-lang/cargo.git" >> version.md
 
 pushd rust
@@ -38,7 +38,7 @@ OPENSSL_STATIC=1 cargo build --release
 popd
 
 if [[ "${HOST_TRIPLE}" != "x86_64-pc-windows-msvc" ]] ; then
-    git clone --single-branch --branch bpf-tools-v1.18 https://github.com/solana-labs/newlib.git
+    git clone --single-branch --branch bpf-tools-v1.19 https://github.com/solana-labs/newlib.git
     echo "$( cd newlib && git rev-parse HEAD )  https://github.com/solana-labs/newlib.git" >> version.md
     mkdir -p newlib_build
     mkdir -p newlib_install
@@ -74,7 +74,7 @@ clang
 clang++
 clang-cl
 clang-cpp
-clang-12
+clang-13
 ld.lld
 ld64.lld
 llc
