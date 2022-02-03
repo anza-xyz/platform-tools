9cbbc8cda620aa4c61099d5f392f83f23b21aa0c
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-02-02 19:27:22 -0800

    Release bpf-tools v1.23

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ rm -rf "${OUT_DIR}"
 mkdir -p "${OUT_DIR}"
 pushd "${OUT_DIR}"
 
-git clone --single-branch --branch bpf-tools-v1.22 https://github.com/solana-labs/rust.git
+git clone --single-branch --branch bpf-tools-v1.23 https://github.com/solana-labs/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
 git clone --single-branch --branch rust-1.56.0 https://github.com/rust-lang/cargo.git
@@ -39,7 +39,7 @@ OPENSSL_STATIC=1 cargo build --release
 popd
 
 if [[ "${HOST_TRIPLE}" != "x86_64-pc-windows-msvc" ]] ; then
-    git clone --single-branch --branch bpf-tools-v1.22 https://github.com/solana-labs/newlib.git
+    git clone --single-branch --branch bpf-tools-v1.23 https://github.com/solana-labs/newlib.git
     echo "$( cd newlib && git rev-parse HEAD )  https://github.com/solana-labs/newlib.git" >> version.md
     mkdir -p newlib_build
     mkdir -p newlib_install
