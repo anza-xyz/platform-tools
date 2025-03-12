efabd8f65a2406b4fd82d80fdcc500525505d4a1
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-03-12 11:05:15 -0300

    Bump platform tools versions (#98)
    * Bump platform tools version
    
    * Do not optimize for size on windows

--- build.sh
+++ build.sh
@@ -65,16 +65,18 @@ rm -rf "${OUT_DIR}"
 mkdir -p "${OUT_DIR}"
 pushd "${OUT_DIR}"
 
-git clone --single-branch --branch solana-tools-v1.44 --recurse-submodules --shallow-submodules https://github.com/anza-xyz/rust.git
+git clone --single-branch --branch solana-tools-v1.45 --recurse-submodules --shallow-submodules https://github.com/anza-xyz/rust.git
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/anza-xyz/rust.git" >> version.md
 
-git clone --single-branch --branch solana-tools-v1.44 https://github.com/anza-xyz/cargo.git
+git clone --single-branch --branch solana-tools-v1.45 https://github.com/anza-xyz/cargo.git
 echo "$( cd cargo && git rev-parse HEAD )  https://github.com/anza-xyz/cargo.git" >> version.md
 
 pushd rust
 if [[ "${HOST_TRIPLE}" == "x86_64-pc-windows-msvc" ]] ; then
     # Do not build lldb on Windows
     sed -i -e 's#enable-projects = \"clang;lld;lldb\"#enable-projects = \"clang;lld\"#g' config.toml
+    # Do not optimize for size on Windows
+    sed -i -e 's#optimize = \"s\"#optimize = true#g' config.toml
 fi
 ./build.sh
 popd
@@ -88,7 +90,7 @@ fi
 popd
 
 if [[ "${HOST_TRIPLE}" != "x86_64-pc-windows-msvc" ]] ; then
-    git clone --single-branch --branch solana-tools-v1.44 https://github.com/anza-xyz/newlib.git
+    git clone --single-branch --branch solana-tools-v1.45 https://github.com/anza-xyz/newlib.git
     echo "$( cd newlib && git rev-parse HEAD )  https://github.com/anza-xyz/newlib.git" >> version.md
 
     build_newlib "v0"
